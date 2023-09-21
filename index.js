809db8225f52e600352d79f5ed6a1956d05c4e24
dtpnst <din454@g.harvard.edu>
2023-09-20 23:06:45 -0400

    Adding comments

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ const fetchColors = ({ name, hex, compName, compHex }) => {
   return fetch(COLORS)
     .then((response) => response.json())
     .then(colors => {
-
+      // Assuming only one argument is passed, just take the first one that's not undefined.
       if(name){
+        // Using includes to filter color names that contains the value passed in.
+        // Setting everything to lowercase to be case insensitive.
         return colors.filter((c) => c.name.toLowerCase().includes(name.toLowerCase()))
       } else if(hex) {
+        // Direct comparison for hex values, setting everything to upper case just to be safe.
         return colors.filter((c) => c.hex.toUpperCase() === hex.toUpperCase())
       } else if(compName) {
+        // For each color's complements, just find the first one that matches the filter.
         return colors.filter((c) => c.comp.find((comp) => comp.name.toLowerCase().includes(compName.toLowerCase())))
       } else if(compHex) {
         return colors.filter((c) => c.comp.find((comp) => comp.hex.toUpperCase() === compHex.toUpperCase()))
       } else {
+        // If no filters are passed in just return everything.
         return colors
       }
 
