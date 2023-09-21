3d8c4cc2eb712ba43f4706e4ff0275f520a8eaa0
dtpnst <din454@g.harvard.edu>
2023-09-20 22:54:51 -0400

    Push with all tests passing

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @returns Promise
  */
 const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+  return fetch(COLORS)
+    .then((response) => response.json())
+    .then(colors => {
+
+      if(name){
+        return colors.filter((c) => c.name.toLowerCase().includes(name.toLowerCase()))
+      } else if(hex) {
+        return colors.filter((c) => c.hex.toUpperCase() === hex.toUpperCase())
+      } else if(compName) {
+        return colors.filter((c) => c.comp.find((comp) => comp.name.toLowerCase().includes(compName.toLowerCase())))
+      } else if(compHex) {
+        return colors.filter((c) => c.comp.find((comp) => comp.hex.toUpperCase() === compHex.toUpperCase()))
+      } else {
+        return colors
+      }
+
+    })
 };
 
 // Leave this here
